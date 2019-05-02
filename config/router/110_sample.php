e93a326ea75420255db6af8626fb3da728a67cc2
Anton Johnsson Håkansson <anton.jh@hotmail.com>
2019-05-02 15:17:15 +0100

    Trying controller-stuff

---
+++ config/router/110_sample.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Routes to ease testing.
+ */
+return [
+    "routes" => [
+        [
+            "info" => "Sample controller app style.",
+            "mount" => "app",
+            "handler" => "\Anax\Controller\SampleAppController",
+        ]
+    ]
+];
