56f1282040276984baf73c2a91a50bf03045b907
Anton Johnsson Håkansson <anton.jh@hotmail.com>
2019-05-02 15:17:45 +0100

    Added oneHundredController to route config

---
+++ config/router/100_onehundred.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Route for OneHundredController.
+ */
+return [
+    "routes" => [
+        [
+            "info" => "OneHundredController.",
+            "mount" => "onehundred-controller",
+            "handler" => "\EVB\OneHundred\OneHundredController",
+        ]
+    ]
+];
