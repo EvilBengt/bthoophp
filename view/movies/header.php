50668995ac65413930b52614ba232dfe60e99894
Anton Johnsson Håkansson <anton.jh@hotmail.com>
2019-05-09 15:33:32 +0100

    Updated header view

--- view/movies/header.php
+++ view/movies/header.php
@@ -1,24 +1,14 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <title><?= $title . $titleExtended ?></title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
+<?php
 
-<navbar class="navbar">
-    <a href="?route=select">SELECT *</a> |
-    <br>
-    <a href="?">Show all movies</a> |
-    <a href="?route=reset">Reset database</a> |
-    <a href="?route=search-title">Search title</a> |
-    <a href="?route=search-year">Search year</a> |
-    <a href="?route=movie-select">Select</a> |
-<!--    <a href="?route=movie-edit">Edit</a> | -->
-    <a href="?route=show-all-sort">Show all sortable</a> |
-    <a href="?route=show-all-paginate">Show all paginate</a> |
-</navbar> 
+namespace Anax\View;
+
+?>
+<nav class="navbar">
+    <a href="all">Show all movies</a> |
+    <a href="search-title">Search title</a> |
+    <a href="search-year">Search year</a> |
+    <a href="create">Create movie</a>
+</nav>
 
 <h1>My Movie Database</h1>
 
