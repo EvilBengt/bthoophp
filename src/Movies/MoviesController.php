ebb0f3fbeda005a9336e22757071bd4b7e78fff1
Anton Johnsson Håkansson <anton.jh@hotmail.com>
2019-05-09 11:59:56 +0100

    Empty controller for movies

---
+++ src/Movies/MoviesController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace EVB\Movies;
+
+use Anax\Commons\AppInjectableInterface;
+use Anax\Commons\AppInjectableTrait;
+
+// use Anax\Route\Exception\ForbiddenException;
+// use Anax\Route\Exception\NotFoundException;
+// use Anax\Route\Exception\InternalErrorException;
+
+/**
+ * A sample controller to show how a controller class can be implemented.
+ * The controller will be injected with $this->app if implementing the interface
+ * AppInjectableInterface, like this sample class does.
+ * The controller is mounted on a particular route and can then handle all
+ * requests for that mount point.
+ *
+ * @SuppressWarnings(PHPMD.TooManyPublicMethods)
+ */
+class MoviesController implements AppInjectableInterface
+{
+    use AppInjectableTrait;
+
+    /**
+     * This is the index method action, it handles:
+     * ANY METHOD mountpoint
+     * ANY METHOD mountpoint/
+     * ANY METHOD mountpoint/index
+     *
+     * @return string
+     */
+    public function indexAction() : string
+    {
+
+    }
+}
