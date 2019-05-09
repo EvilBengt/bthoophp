9a7686086fa45e7d1a1561b19d8ac83d341cb4e3
Anton Johnsson Håkansson <anton.jh@hotmail.com>
2019-05-09 15:34:21 +0100

    Updated edit view links

--- view/movies/movie-edit.php
+++ view/movies/movie-edit.php
@@ -1,21 +1,26 @@
+<?php
+
+namespace Anax\View;
+
+?>
 <form method="post">
     <fieldset>
     <legend>Edit</legend>
     <input type="hidden" name="movieId" value="<?= $movie->id ?>"/>
 
     <p>
-        <label>Title:<br> 
+        <label>Title:<br>
         <input type="text" name="movieTitle" value="<?= $movie->title ?>"/>
         </label>
     </p>
 
     <p>
-        <label>Year:<br> 
+        <label>Year:<br>
         <input type="number" name="movieYear" value="<?= $movie->year ?>"/>
     </p>
 
     <p>
-        <label>Image:<br> 
+        <label>Image:<br>
         <input type="text" name="movieImage" value="<?= $movie->image ?>"/>
         </label>
     </p>
@@ -23,10 +28,10 @@
     <p>
         <input type="submit" name="doSave" value="Save">
         <input type="reset" value="Reset">
+        <input type="submit" name="doDelete" value="DELETE">
     </p>
     <p>
-        <a href="?route=movie-select">Select movie</a> |
-        <a href="?">Show all</a>
+        <a href="all">Show all</a>
     </p>
     </fieldset>
 </form>
