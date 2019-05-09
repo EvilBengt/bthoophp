f494d283bed77927e88528bd1a74067fd3781bb0
Anton Johnsson Håkansson <anton.jh@hotmail.com>
2019-05-09 15:33:09 +0100

    Finished MoviesController with routes for all, search-title, search-year, edit, create

--- src/Movies/MoviesController.php
+++ src/Movies/MoviesController.php
@@ -23,6 +23,13 @@ class MoviesController implements AppInjectableInterface
     use AppInjectableTrait;
 
     /**
+     * String to append to end of all titles.
+     *
+     * @var string
+     */
+    private $titleExtended = " | My Movie Database";
+
+    /**
      * This is the index method action, it handles:
      * ANY METHOD mountpoint
      * ANY METHOD mountpoint/
@@ -30,8 +37,177 @@ class MoviesController implements AppInjectableInterface
      *
      * @return string
      */
-    public function indexAction() : string
+    public function indexAction() : object
     {
+        return $this->app->response->redirect("movies/all");
+    }
 
+    /**
+     * List all movies
+     * ANY METHOD mountpoint/all
+     *
+     * @return object
+     */
+    public function allAction() : object
+    {
+        $this->app->db->connect();
+
+        $sql = "SELECT * FROM movie;";
+        $res = $this->app->db->executeFetchAll($sql);
+
+        $this->app->page->add("movies/header");
+        $this->app->page->add("movies/show-all", ["resultset" => $res]);
+
+        return $this->app->page->render([
+            "title" => "Show all" . $this->titleExtended
+        ]);
+    }
+
+    /**
+     * Search for title
+     * ANY METHOD mountpoint/search-title
+     *
+     * @return object
+     */
+    public function searchTitleAction() : object
+    {
+        $search = $this->app->request->getGet("search");
+
+        $this->app->db->connect();
+
+        $sql = "SELECT * FROM movie WHERE title LIKE ?";
+
+        $res = $this->app->db->executeFetchAll($sql, [$search]);
+
+        $this->app->page->add("movies/header");
+        $this->app->page->add("movies/search-title", ["search" => $search]);
+        if (\count($res) > 0) {
+            $this->app->page->add("movies/show-all", ["resultset" => $res]);
+        }
+
+        return $this->app->page->render([
+            "title" => "Search title" . $this->titleExtended
+        ]);
+    }
+
+    /**
+     * Search for year intervall
+     * ANY METHOD mountpoint/search-year
+     *
+     * @return object
+     */
+    public function searchYearAction() : object
+    {
+        $year1 = $this->app->request->getGet("year1");
+        $year2 = $this->app->request->getGet("year2");
+
+        $this->app->db->connect();
+
+        $sql = "SELECT * FROM movie WHERE year >= ? AND year <= ?";
+
+        $res = $this->app->db->executeFetchAll($sql, [
+            $year1,
+            $year2
+        ]);
+
+        $this->app->page->add("movies/header");
+        $this->app->page->add("movies/search-year", [
+            "year1" => $year1,
+            "year2" => $year2
+        ]);
+        if (\count($res) > 0) {
+            $this->app->page->add("movies/show-all", ["resultset" => $res]);
+        }
+
+        return $this->app->page->render([
+            "title" => "Search title" . $this->titleExtended
+        ]);
+    }
+
+    /**
+     * Edit a movie
+     * GET mountpoint/edit
+     *
+     * @return object
+     */
+    public function editActionGet() : object
+    {
+        $this->app->db->connect();
+
+        $sql = "SELECT * FROM movie WHERE id = ?;";
+        $res = $this->app->db->executeFetch($sql, [$this->app->request->getGet("id")]);
+
+        $this->app->page->add("movies/header");
+        $this->app->page->add("movies/movie-edit", ["movie" => $res]);
+
+        return $this->app->page->render([
+            "title" => "Edit movie" . $this->titleExtended
+        ]);
+    }
+
+    /**
+     * Edit a movie, do the stuff
+     * POST mountpoint/edit
+     *
+     * @return object
+     */
+    public function editActionPost() : object
+    {
+        $this->app->db->connect();
+
+        $movieId    = $this->app->request->getPost("movieId");
+        $movieTitle = $this->app->request->getPost("movieTitle");
+        $movieYear  = $this->app->request->getPost("movieYear");
+        $movieImage = $this->app->request->getPost("movieImage");
+
+        if ($this->app->request->getPost("doDelete")) {
+            $sql = "DELETE FROM movie WHERE id = ?;";
+            $this->app->db->execute($sql, [$movieId]);
+        } else if ($this->app->request->getPost("doSave")) {
+            $sql = "UPDATE movie SET title = ?, year = ?, image = ? WHERE id = ?;";
+            $this->app->db->execute($sql, [$movieTitle, $movieYear, $movieImage, $movieId]);
+        }
+
+        return $this->app->response->redirect("movies/all");
+    }
+
+    /**
+     * Create a movie
+     * GET mountpoint/create
+     *
+     * @return object
+     */
+    public function createActionGet() : object
+    {
+        $this->app->page->add("movies/header");
+        $this->app->page->add("movies/movie-create");
+
+        return $this->app->page->render([
+            "title" => "Create movie" . $this->titleExtended
+        ]);
+    }
+
+    /**
+     * Create a movie, do the stuff
+     * POST mountpoint/create
+     *
+     * @return object
+     */
+    public function createActionPost() : object
+    {
+        $this->app->db->connect();
+
+        $movieTitle = $this->app->request->getPost("movieTitle");
+        $movieYear  = $this->app->request->getPost("movieYear");
+        $movieImage = $this->app->request->getPost("movieImage");
+
+        $sql = "INSERT INTO movie (title, year, image) VALUES (?, ?, ?)";
+        $this->app->db->execute($sql, [
+            $movieTitle,
+            $movieYear,
+            $movieImage
+        ]);
+
+        return $this->app->response->redirect("movies/all");
     }
 }
