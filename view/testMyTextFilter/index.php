a86c8ce5284435be4c9cc6187e7639bb0ea488ae
Anton Johnsson Håkansson <anton.jh@hotmail.com>
2019-05-16 15:32:21 +0100

    View for MyTextFilter test page

---
+++ view/testMyTextFilter/index.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Anax\View;
+
+?>
+<h1>MyTextFilter testsida</h1>
+
+
+
+<h2>bbcode</h2>
+
+<h3>Källa</h3>
+<pre>
+<?= $bbcodeRaw ?>
+</pre>
+
+<h3>HTML</h3>
+<pre>
+<?= $bbcodeHTML ?>
+</pre>
+
+<h3>Resultat</h3>
+<?= $bbcodeResult ?>
+
+
+
+<h2>link</h2>
+
+<h3>Källa</h3>
+<pre>
+<?= $linkRaw ?>
+</pre>
+
+<h3>HTML</h3>
+<pre>
+<?= $linkHTML ?>
+</pre>
+
+<h3>Resultat</h3>
+<?= $linkResult ?>
+
+
+
+
+<h2>markdown</h2>
+
+<h3>Källa</h3>
+<pre>
+<?= $markdownRaw ?>
+</pre>
+
+<h3>HTML</h3>
+<pre>
+<?= $markdownHTML ?>
+</pre>
+
+<h3>Resultat</h3>
+<?= $markdownResult ?>
+
+
+
+<h2>nl2br</h2>
+
+<h3>Källa</h3>
+<pre>
+<?= $nl2brRaw ?>
+</pre>
+
+<h3>HTML</h3>
+<pre>
+<?= $nl2brHTML ?>
+</pre>
+
+<h3>Resultat</h3>
+<?= $nl2brResult ?>
